8009270c2d4ba7a9d5fc1110c68af1b6d1ecf6f8
heabyfik <rafikova0rr@gmail.com>
2019-05-02 21:24:18 +0700

    wrote button for gallery

--- app/src/main/java/com/example/pain_1/MainActivity.kt
+++ app/src/main/java/com/example/pain_1/MainActivity.kt
@@ -1,30 +1,83 @@
-package com.example.pain_1
+package codestart.info.kotlinphoto
 
-import android.support.v7.app.AppCompatActivity
+import android.Manifest
+import android.app.Activity
+import android.content.ContentValues
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
+import android.support.v7.app.AlertDialog
+import android.support.v7.app.AppCompatActivity
+import android.webkit.PermissionRequest
+import android.widget.Toast
+
 import kotlinx.android.synthetic.main.activity_main.*
 
+
 class MainActivity : AppCompatActivity() {
+
+    var fileUri: Uri? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // Example of a call to a native method
-        sample_text.text = stringFromJNI()
+        //listen to gallery button click
+        gallery.setOnClickListener {
+            pickPhotoFromGallery()
+        }
+
+        //listen to take photo button click
+       takePhoto.setOnClickListener {
+            //askCameraPermission()
+        }
+
+    }
+    //pick a photo from gallery
+    private fun pickPhotoFromGallery() {
+        val pickImageIntent = Intent(Intent.ACTION_PICK,
+            MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+
+        startActivityForResult(pickImageIntent, AppConstants.PICK_PHOTO_REQUEST)
     }
 
-    /**
-     * A native method that is implemented by the 'native-lib' native library,
-     * which is packaged with this application.
-     */
-    external fun stringFromJNI(): String
-
-    companion object {
-
-        // Used to load the 'native-lib' library on application startup.
-        init {
-            System.loadLibrary("native-lib")
+    //launch the camera to take photo via intent
+    private fun launchCamera() {
+        val values = ContentValues(1)
+        values.put(MediaStore.Images.Media.MIME_TYPE, "image/jpg")
+        fileUri = contentResolver
+            .insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+                values)
+        val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        if(intent.resolveActivity(packageManager) != null) {
+            intent.putExtra(MediaStore.EXTRA_OUTPUT, fileUri)
+            intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION
+                    or Intent.FLAG_GRANT_WRITE_URI_PERMISSION)
+            startActivityForResult(intent, AppConstants.TAKE_PHOTO_REQUEST)
         }
     }
+
+    //ask for permission to take photo
+   
+
+    //override function that is called once the photo has been taken
+    override fun onActivityResult(requestCode: Int, resultCode: Int,
+                                  data: Intent?) {
+        if (resultCode == Activity.RESULT_OK
+            && requestCode == AppConstants.TAKE_PHOTO_REQUEST) {
+            //photo from camera
+            //display the photo on the imageview
+            imageView.setImageURI(fileUri)
+        }else if(resultCode == Activity.RESULT_OK
+            && requestCode == AppConstants.PICK_PHOTO_REQUEST){
+            //photo from gallery
+            fileUri = data?.data
+            imageView.setImageURI(fileUri)
+        } else {
+            super.onActivityResult(requestCode, resultCode, data)
+        }
+    }
+
+
 }
