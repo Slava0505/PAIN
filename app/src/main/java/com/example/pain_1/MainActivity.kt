5c8f738b2ba462e0e2086d22e72f546847a9f4aa
nianas <anastasiya@politova.pro>
2019-05-25 02:10:25 +0700

    Add take photo

--- app/src/main/java/com/example/pain_1/MainActivity.kt
+++ app/src/main/java/com/example/pain_1/MainActivity.kt
@@ -1,25 +1,28 @@
 package codestart.info.kotlinphoto
 
-import android.Manifest
 import android.app.Activity
 import android.content.Intent
-import android.content.SharedPreferences
 import android.net.Uri
+import android.content.Context
 import android.os.Bundle
+import android.os.Environment
 import android.provider.MediaStore
+import android.support.v4.content.FileProvider
 import android.support.v7.app.AppCompatActivity
-import android.view.View
 import com.example.pain_1.AStarActivity
 import com.example.pain_1.EditorActivity
-import kotlinx.android.synthetic.main.activity_editor.imageEditorView
-import kotlinx.android.synthetic.main.activity_editor.*
 import kotlinx.android.synthetic.main.activity_main.*
+import java.io.File
+import java.io.IOException
+import java.text.SimpleDateFormat
+import java.util.*
 
 
 class MainActivity : AppCompatActivity() {
 
     var fileUri: Uri? = null
-
+    val REQUEST_TAKE_PHOTO = 2
+    var mCurrentPhotoPath: String? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -32,7 +35,7 @@ class MainActivity : AppCompatActivity() {
 
         //listen to take photo button click
         takePhoto.setOnClickListener {
-            askCameraPermission()
+            takeAndSetPhoto()
         }
         //listen to A* button click
         aStar.setOnClickListener {
@@ -51,14 +54,45 @@ class MainActivity : AppCompatActivity() {
 
 
 
-    private fun askCameraPermission() {
+  /*  private fun askCameraPermission() {
         Intent(MediaStore.ACTION_IMAGE_CAPTURE).also { takePictureIntent ->
             takePictureIntent.resolveActivity(packageManager)?.also {
                 startActivityForResult(takePictureIntent,  AppConstants.TAKE_PHOTO_REQUEST)
             }
         }
-    }
+    }*/
+    private fun createImageFile(): File {
+        // Create an image file name
+        val timeStamp = SimpleDateFormat("yyyyMMdd_HHmmss").format(Date())
+        val imageFileName = "JPEG_" + timeStamp + "_"
+        val storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES)
+        val image = File.createTempFile(
+            imageFileName, /* prefix */
+            ".jpg", /* suffix */
+            storageDir      /* directory */
+        )
 
+        // Save a file: path for use with ACTION_VIEW intents
+        mCurrentPhotoPath = image.absolutePath
+        return image
+    }
+    private fun takeAndSetPhoto() {
+        val takePictureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        if (takePictureIntent.resolveActivity(packageManager) != null) {
+            var photoFile: File? = null
+            try {
+                photoFile = createImageFile()
+            } catch (e: IOException) {
+                e.printStackTrace()
+            }
+            if (photoFile != null) {
+                val auth: String = packageName + ".fileprovider"
+                val photoURI = Intent(this, EditorActivity::class.java)
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI)
+                startActivityForResult(takePictureIntent, REQUEST_TAKE_PHOTO)
+            }
+        }
+    }
 
     //override function that is called once the photo has been taken
     override fun onActivityResult(requestCode: Int, resultCode: Int,
