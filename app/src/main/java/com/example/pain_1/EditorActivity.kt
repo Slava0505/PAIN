bc13f090120bcfb50df835877384fd2ae1af5cdb
Slava0505 <s>
2019-05-25 02:02:34 +0700

    Fixed

--- app/src/main/java/com/example/pain_1/EditorActivity.kt
+++ app/src/main/java/com/example/pain_1/EditorActivity.kt
@@ -7,8 +7,12 @@ import android.os.Bundle
 import codestart.info.kotlinphoto.R
 import kotlinx.android.synthetic.main.activity_editor.*
 import kotlinx.android.synthetic.main.activity_main.*
+import android.app.Activity
+import android.provider.MediaStore
+import codestart.info.kotlinphoto.AppConstants
 
 class EditorActivity : AppCompatActivity() {
+    var fileUri: Uri? = null
     companion object {
 
         const val TOTAL_COUNT = "total_count"
@@ -45,12 +49,48 @@ class EditorActivity : AppCompatActivity() {
             resiz.data= this.intent.data
             startActivity(resiz)
         }
+        gallery_button.setOnClickListener {
+            pickPhotoFromGallery()
+        }
+    }
+
+    fun pickPhotoFromGallery() {
+        val pickImageIntent = Intent(Intent.ACTION_PICK,
+            MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+
+        startActivityForResult(pickImageIntent, AppConstants.PICK_PHOTO_REQUEST)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int,
+                                  data: Intent?) {
+        if (resultCode == Activity.RESULT_OK
+            && requestCode == AppConstants.TAKE_PHOTO_REQUEST) {
+//photo from camera
+//display the photo on the imageview
+            fileUri = data?.data
+            startEditor(fileUri)
+
+        }else if(resultCode == Activity.RESULT_OK
+            && requestCode == AppConstants.PICK_PHOTO_REQUEST){
+//photo from gallery
+            fileUri = data?.data
+            startEditor(fileUri)
+        } else {
+            super.onActivityResult(requestCode, resultCode, data)
+        }
+    }
+    fun startEditor (fileUri: Uri?) {
+        val editor = Intent(this, EditorActivity::class.java)
+        editor.data = fileUri
+        startActivity(editor)
     }
 
 
 
+
+
     fun showStartPhoto() {
-        // Get the count from the intent extras
+// Get the count from the intent extras
         val fileUri = intent.getData()
         imageEditorView.setImageURI(fileUri)
     }
